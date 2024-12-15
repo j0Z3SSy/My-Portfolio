55634e279ccddd88562d8c7594bb4f054a3ee4af
Joni Mitronen <joni.mitronen@gmail.com>
2024-12-15 19:35:30 +0200

    go git

---
+++ javascript/includes.js
@@ -0,0 +1,12 @@
+// includes.js
+function includeHTML(id, filePath) {
+  fetch(filePath)
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById(id).innerHTML = data;
+    });
+}
+
+// Call the function to include the navbar and footer
+includeHTML("navbar", "../html/page-includes/navbar.html");
+includeHTML("footer", "../html/page-includes/footer.html");
