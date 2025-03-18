9b705561c05796fd4e258fd912e8b27b386f4abe
Joni Mitronen <joni.mitronen@gmail.com>
2025-03-18 19:17:53 +0200

    edits

--- javascript/includes.js
+++ javascript/includes.js
@@ -1,6 +1,19 @@
+// function includeHTML(id, filePath) {
+//   fetch(filePath)
+//     .then((response) => response.text())
+//     .then((data) => {
+//       document.getElementById(id).innerHTML = data;
+//     });
+// }
+
+// // Call the function to include the navbar and footer
+// includeHTML("navbar", "html/page-includes/navbar.html");
+// includeHTML("footer", "html/page-includes/footer.html");
+
 // includes.js
 function includeHTML(id, filePath) {
-  fetch(filePath)
+  let basePath = window.location.pathname.includes("html/") ? "../" : "./";
+  fetch(basePath + filePath)
     .then((response) => response.text())
     .then((data) => {
       document.getElementById(id).innerHTML = data;
