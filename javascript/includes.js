b727fc0aabfbb04c398a830ee3141765bc9ca15a
Joni Mitronen <joni.mitronen@gmail.com>
2025-03-18 19:20:18 +0200

    update includes.js

--- javascript/includes.js
+++ javascript/includes.js
@@ -1,25 +1,26 @@
-// function includeHTML(id, filePath) {
-//   fetch(filePath)
-//     .then((response) => response.text())
-//     .then((data) => {
-//       document.getElementById(id).innerHTML = data;
-//     });
-// }
-
-// // Call the function to include the navbar and footer
-// includeHTML("navbar", "html/page-includes/navbar.html");
-// includeHTML("footer", "html/page-includes/footer.html");
-
 // includes.js
 function includeHTML(id, filePath) {
-  let basePath = window.location.pathname.includes("html/") ? "../" : "./";
-  fetch(basePath + filePath)
+  fetch(filePath)
     .then((response) => response.text())
     .then((data) => {
       document.getElementById(id).innerHTML = data;
     });
 }
 
+// // Call the function to include the navbar and footer
+// includeHTML("navbar", "html/page-includes/navbar.html");
+// includeHTML("footer", "html/page-includes/footer.html");
+
+// includes.js
+// function includeHTML(id, filePath) {
+//   let basePath = window.location.pathname.includes("html/") ? "../" : "./";
+//   fetch(basePath + filePath)
+//     .then((response) => response.text())
+//     .then((data) => {
+//       document.getElementById(id).innerHTML = data;
+//     });
+// }
+
 // Call the function to include the navbar and footer
-includeHTML("navbar", "html/page-includes/navbar.html");
-includeHTML("footer", "html/page-includes/footer.html");
+includeHTML("navbar", "/My-Portfolio/html/page-includes/navbar.html");
+includeHTML("footer", "/My-Portfolio/html/page-includes/footer.html");
