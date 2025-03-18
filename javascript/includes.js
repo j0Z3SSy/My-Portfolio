6d957213ff3daf5449f8d8b89deb9e809e34d9dc
Joni Mitronen <joni.mitronen@gmail.com>
2025-03-18 19:27:31 +0200

    removed useless code

--- javascript/includes.js
+++ javascript/includes.js
@@ -7,20 +7,5 @@ function includeHTML(id, filePath) {
     });
 }
 
-// // Call the function to include the navbar and footer
-// includeHTML("navbar", "html/page-includes/navbar.html");
-// includeHTML("footer", "html/page-includes/footer.html");
-
-// includes.js
-// function includeHTML(id, filePath) {
-//   let basePath = window.location.pathname.includes("html/") ? "../" : "./";
-//   fetch(basePath + filePath)
-//     .then((response) => response.text())
-//     .then((data) => {
-//       document.getElementById(id).innerHTML = data;
-//     });
-// }
-
-// Call the function to include the navbar and footer
 includeHTML("navbar", "/My-Portfolio/html/page-includes/navbar.html");
 includeHTML("footer", "/My-Portfolio/html/page-includes/footer.html");
